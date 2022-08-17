4f72d184c5143942866f5514884218d3ae8ae85f
mmanmaniac21@ <mmanmaniac21@gmail.com>
2022-08-17 14:41:49 +1000

    Testing pushing

--- myapp.py
+++ myapp.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Testing123</h1><br>Congratulations! This is the flask webpage now."
+    return "<h1>I changed this text to test something</h1><br>Congratulations! This is the flask webpage now."
 
 if __name__ == "__main__":
     app.run()
