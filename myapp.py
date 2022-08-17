8208aa25264667a78a0348cd36475d5fc0ae230e
geebung02 <65688533+geebung02@users.noreply.github.com>
2022-08-17 21:30:15 +1000

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -7,7 +7,7 @@ def home():
     return "<h1>I changed this text to test something</h1><br>Congratulations! This is the flask webpage now."
 
 @app.route("/map")
-def home():
+def map():
     start_coords = (-27.4705, 153.0260) ##Brisbane long/lat
     folium_map = folium.Map(location=start_coords, zoom_start=14)
     return folium_map._repr_html_()
