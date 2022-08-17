05cf84ed069f4634435b6888a5eb2bc356af6b45
geebung02 <65688533+geebung02@users.noreply.github.com>
2022-08-17 21:14:20 +1000

    Update testmap.py

--- testmap.py
+++ testmap.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def home():
     start_coords = (-27.4705, 153.0260) ##Brisbane long/lat
     folium_map = folium.Map(location=start_coords, zoom_start=14)
     return folium_map._repr_html_()
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
