d7239cd293933e4b402e8c16dde32821dabc7cac
mmanmaniac <mmanmaniac21@gmail.com>
2022-08-21 14:24:59 +1000

    Testing folium

--- myapp.py
+++ myapp.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+import folium
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>I changed this text to test something</h1><br>Congratulations! This is the flask webpage now."
+    return render_template("index.html")
+
+@app.route("/test")
+def test():
+    coords = (46, 142)
+    map = folium.Map(location=coords, zoom_start=14)
+    return render_template("test.html") + map._repr_html_()
 
 if __name__ == "__main__":
     app.run()
