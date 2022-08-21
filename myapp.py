ca463c1e4fda86e478673f5c2bab1e2f9698ef38
mmanmaniac <mmanmaniac21@gmail.com>
2022-08-21 18:13:52 +1000

    Built out the general website layout. Used some bootstrap navigation bar to get things started. Made a page to test charts, have a functional chart.

--- myapp.py
+++ myapp.py
@@ -5,13 +5,29 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("header.html") + render_template("index.html")
+
+@app.route("/calculator")
+def calculator():
+    return render_template("header.html") + render_template("calculator.html")
+
+@app.route("/guide")
+def guide():
+    return render_template("header.html") + render_template("guide.html")
+
+@app.route("/resources")
+def resources():
+    return render_template("header.html") + render_template("resources.html")
+
+@app.route("/charts")
+def charts():
+    return render_template("header.html") + render_template("charts.html")
 
 @app.route("/test")
 def test():
     coords = (46, 142)
     map = folium.Map(location=coords, zoom_start=14)
-    return render_template("test.html") + map._repr_html_()
+    return render_template("header.html") + render_template("test.html") + map._repr_html_()
 
 if __name__ == "__main__":
     app.run()
