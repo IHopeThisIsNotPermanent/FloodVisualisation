ea00e8fbd5e3d8f99f6b02ba4d3ea910ccf0f2aa
geebung02 <65688533+geebung02@users.noreply.github.com>
2022-09-14 12:52:03 +1000

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -38,7 +38,10 @@ def test():
         ##location coords are location.longitude, location.latitude
         #TODO: only accept location within Wivenhoe region - need coords of contour region
         #if location not None: <- TODO: fix None exception 
-        return render_template("header.html") + render_template("contour.html", lat = location.latitude, long = location.longitude) 
+        try:
+            return render_template("header.html") + render_template("contour.html", lat = location.latitude, long = location.longitude) 
+        except:
+            pass
         #return f"<h1>{test}</h1><br><img src='https://cdn.britannica.com/44/4144-004-43DD2776/Peneus-setiferus.jpg'>"
     
     #default coords to 'brisbane' == Elizabeth St
