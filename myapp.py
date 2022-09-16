da173478dba84192c2ce4a99f2b4d9e74cff6a80
mmanmaniac <mmanmaniac21@gmail.com>
2022-09-16 14:44:37 +1000

    Homepage changes with bootstrap.

--- myapp.py
+++ myapp.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template, request
 import folium
 from folium import GeoJson, plugins
 from folium.plugins import HeatMap
-import geopy 
-from geopy import Nominatim 
+import geopy
+from geopy import Nominatim
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("header.html") + render_template("index.html")
+    return render_template("header.html") + render_template("home.html")
 
 @app.route("/calculator")
 def calculator():
@@ -37,13 +37,13 @@ def test():
         location = locator.geocode(test)
         ##location coords are location.longitude, location.latitude
         #TODO: only accept location within Wivenhoe region - need coords of contour region
-        #if location not None: <- TODO: fix None exception 
+        #if location not None: <- TODO: fix None exception
         try:
-            return render_template("header.html") + render_template("contour.html", lat = location.latitude, long = location.longitude) 
+            return render_template("header.html") + render_template("contour.html", lat = location.latitude, long = location.longitude)
         except:
             return render_template("header.html") + render_template("calculator.html")
         #return f"<h1>{test}</h1><br><img src='https://cdn.britannica.com/44/4144-004-43DD2776/Peneus-setiferus.jpg'>"
-    
+
     #default coords to 'brisbane' == Elizabeth St
     return render_template("header.html") + render_template("contour.html", lat = -27.4705, long = 153.0260 )
 
